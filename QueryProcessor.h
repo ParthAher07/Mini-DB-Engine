8fff875694ea3ee0071ebb690fbd133e93c7a8c3
ParthAher07 <parthaher2005@gmail.com>
2025-05-29 03:42:52 +0530

    diff header file for processor

---
+++ QueryProcessor.h
@@ -0,0 +1,178 @@
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+
+class QueryProcessor
+{
+private:
+    set<string> keywords = {
+        "create","table","primary",
+        "key","int","varchar",
+        "date","decimal","drop","describe",
+        "insert","into","values",
+        "help","tables","select","from","where",
+        "and","or",
+    };
+    vector<string> Tokens;
+
+public:
+    QueryProcessor(){};
+    void run(const string &query);
+    void ParseIntoTokens(const string &query);
+    void ToSmallerCase();
+    void DisplayTokens() const;
+    void executeQuery();
+};
+
+void QueryProcessor::ParseIntoTokens(const string &Query)
+{
+    string temp = "";
+
+    for (int i = 0; i < Query.size(); i++)
+    {
+        char c = Query[i];
+
+        if (c == '"')
+        {
+            i++;
+            while (Query[i] != '"')
+            {
+                temp += Query[i];
+                i++;
+            }
+
+            if (temp != "")
+                Tokens.push_back(temp);
+            temp = "";
+        }
+        else if (c == ' ' || c == '(' || c == ')' || c == ',' || c == ';' || c == '*')
+        {
+            if (temp != "")
+                Tokens.push_back(temp);
+            if (c == '*') // for handling special case in select
+                Tokens.push_back("*");
+            temp = "";
+        }
+        else if (Query[i] == '!' && Query[i + 1] == '=')
+        {
+            if (temp != "")
+                Tokens.push_back(temp);
+            temp = "";
+
+            Tokens.push_back("!=");
+            i++;
+        }
+        else if (c == '<' || c == '>' || c == '=') // we can add other operators if we want to
+        {
+            if (temp != "")
+                Tokens.push_back(temp);
+            temp = "";
+
+            Tokens.push_back(string(1, c));
+        }
+
+        else
+        {
+            temp += c;
+        }
+    }
+    if (temp != "")
+        Tokens.push_back(temp);
+}
+
+void QueryProcessor::ToSmallerCase()
+{
+    for (int i = 0; i < Tokens.size(); i++)
+    {
+        string temp = Tokens[i];
+        transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
+        if (keywords.find(temp) != keywords.end())
+            Tokens[i] = temp;
+    }
+}
+
+void QueryProcessor::DisplayTokens() const
+{
+    cout << "Tokens: ";
+    for (const string &token : Tokens)
+    {
+        cout << token << " ";
+    }
+    cout << endl;
+}
+
+// void QueryProcessor::executeQuery()
+// {
+//     if (Tokens.empty())
+//         return;
+
+//     if (Tokens[0] == "create" && Tokens[1] == "table")
+//     {
+//         CreateTable(Tokens);
+//     }
+
+//     else if (Tokens[0] == "drop" && Tokens[1] == "table")
+//     {
+//         DropTable(Tokens);
+//     }
+
+//     else if (Tokens[0] == "describe")
+//     {
+//         DescribeTable(Tokens);
+//     }
+
+//     else if (Tokens[0] == "help" && Tokens[1] == "tables")
+//     {
+//         HelpTables();
+//     }
+
+//     else if (Tokens[0] == "help")
+//     {
+//         HelpCommand(Tokens);
+//     }
+
+//     else if (Tokens[0] == "insert" && Tokens[1] == "into")
+//     {
+//         int res = InsertInto(Tokens);
+//         res == 1 ? cout << "Tuple inserted successfully" << endl : cout << "Tuple not inserted" << endl;
+//     }
+
+//     else if (Tokens[0] == "select")
+//     {
+//         Select(Tokens);
+//     }
+
+//     else if (Tokens[0] == "update")
+//     {
+//         UpdateTable(Tokens);
+//     }
+
+//     else if (Tokens[0] == "delete" && Tokens[1] == "from")
+//     {
+//         DeleteFrom(Tokens);
+//     }
+
+//     else if (Tokens[0] == "quit")
+//     {
+//         cout << "Program terminated successfully." << endl;
+//         exit(0);
+//     }
+//     else
+//     {
+//         cout << "INVALID QUERY" << endl;
+//     }
+// };
+
+void QueryProcessor::run(const string &Query)
+{
+    if (Query.back() != ';')
+    {
+        cout << "; missing at the end" << endl;
+    }
+    else
+    {
+        ParseIntoTokens(Query);
+        ToSmallerCase();
+        DisplayTokens();
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,95 +1,6 @@
 #include<bits/stdc++.h>
+#include "QueryProcessor.h" 
 using namespace std;
-
-set<string> keywords = {
-    "create","table","primary","key",
-    "int","varchar","date","decimal"
-    "drop","describe",
-    "insert","into","values",
-    "help","tables",
-    "select","from","where","and","or",
-};
-
-vector<string> Tokens;
-
-void DisplayTokens() {
-    cout<<"Tokens: "<<endl;
-    if(Tokens.size() == 0) {
-        cout<<"No tokens found."<<endl;
-        return;
-    }
-    for(string x:Tokens) cout<<x<<endl;
-}
-
-void ParseIntoTokens(string Query)
-{
-    string temp="";
-
-    for(int i=0;i<Query.size();i++)
-    {
-        char c = Query[i];
-
-        if(c=='"')
-        {
-            i++;
-            while(Query[i]!='"')
-            {
-                temp+=Query[i];
-                i++;
-            }
-            
-            if(temp!="")
-                Tokens.push_back(temp);
-            temp = "";
-        }
-        else if(c==' ' || c=='(' || c==')' || c==',' || c==';' || c=='*')
-        {
-            if(temp!="")
-                Tokens.push_back(temp);
-            if(c=='*')//for handling special case in select
-                Tokens.push_back("*");
-            temp = "";
-        }
-        else if(Query[i]=='!' && Query[i+1]=='=')
-        {
-            if(temp!="")
-                Tokens.push_back(temp);
-            temp = "";
-
-            Tokens.push_back("!=");
-            i++;
-        }
-        else if(c=='<' || c=='>'|| c=='=')//we can add other operators if we want to
-        {
-            if(temp!="")
-                Tokens.push_back(temp);
-            temp = "";
-
-            Tokens.push_back(string(1,c));
-        }
-        
-        else 
-        {
-            temp += c; 
-        }
-    }
-    if(temp!="")
-        Tokens.push_back(temp);
-}
-
-void Execute(){
-
-}
-
-void ToSmallerCase(){
-
-    for(int i = 0;i<Tokens.size();i++){
-        string temp = Tokens[i];
-        transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
-        if(keywords.find(temp) != keywords.end()) Tokens[i] = temp;
-    }
-
-}
 
 int main(){
 
@@ -97,23 +8,16 @@ int main(){
     string Query;
 
     while(1){
-        Tokens.clear();
         //attributes_of_table.clear();
         cout<<endl<<">> ";
 
         getline(cin,Query); 
         cout<<endl;
-        
-        if(Query.back()!=';') 
-        {
-            cout<<"; missing at the end"<<endl;
-            continue;
-        }
 
+        QueryProcessor qp;
+        qp.run(Query);
         
-        ParseIntoTokens(Query);
-        ToSmallerCase();
-        DisplayTokens();
+  
         // bool noerrors = ErrorsChecking(Tokens);
 
         // if(noerrors)
